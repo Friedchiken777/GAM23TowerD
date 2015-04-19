39c2096603d8bc5fb63a4246eadb7ebb891901d4
Friedchiken777 <Friedchiken777@gmail.com>
2015-04-18 21:43:49 -0700

    Grid Locking Added. Very start of path finding, but wanted to make the commit...
    -Will

---
+++ Assets/Scripts/LockToGrid.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LockToGrid : MonoBehaviour 
+{
+	Vector3 tempPos;
+	
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		tempPos = transform.position;
+		if(tempPos.x % 1 != 0 || tempPos.y % 1 != 0 || tempPos.z % 1 != 0)
+		{
+			Vector3 newPos = new Vector3(Mathf.Floor(tempPos.x),Mathf.Floor(tempPos.y),Mathf.Floor(tempPos.z));
+			transform.position = newPos;
+		}
+	}
+}
