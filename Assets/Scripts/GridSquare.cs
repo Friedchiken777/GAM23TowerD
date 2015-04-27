21b283beae020a80e1cfb4ac7dbd941e05c831b0
Will George School <FriedChiken777@gmail.com>
2015-04-27 13:15:26 -0700

    Couple Fixes to help with level building
    -Will

--- Assets/Scripts/GridSquare.cs
+++ Assets/Scripts/GridSquare.cs
@@ -48,7 +48,7 @@ public class GridSquare : MonoBehaviour
 	
 	public void SetNeighbors()
 	{
-		Collider[] hitColliders = Physics.OverlapSphere(gameObject.transform.position, 0.5f);
+		Collider[] hitColliders = Physics.OverlapSphere(gameObject.transform.position, 0.7f);
 		int i = 0;
 		while (i < hitColliders.Length) 
 		{
@@ -63,7 +63,7 @@ public class GridSquare : MonoBehaviour
 	public void SetCanBuildDefaults()
 	{
 		Vector3 up = transform.TransformDirection(Vector3.up);
-		if (Physics.Raycast(transform.position, up, 5))
+		if (Physics.Raycast(transform.position, up, 1.5f))
 		{
 			canBuild = canMove = false;
 		}
