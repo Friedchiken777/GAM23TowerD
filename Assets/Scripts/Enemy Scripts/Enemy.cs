245e2ef6bfcccfc7d637a76d60ce750af9864055
Ryan Zagala <gman10501@gmail.com>
2015-05-05 23:31:14 -0700

    Particle Systems programmed for instantiation during gameplay. Enemy Model prefabs are quite buggy however.

--- Assets/Scripts/Enemy Scripts/Enemy.cs
+++ Assets/Scripts/Enemy Scripts/Enemy.cs
@@ -7,25 +7,79 @@ public class Enemy : MonoBehaviour
     public float attackTimer = 0.0f;
     public EnemyType enemyType;
     public Transform target;
+    public GameObject[] particlePlacements;
     public AudioClip[] audioClip;
 
 	// Use this for initialization
 	void Start () 
+    {
+        SetParticles();
+	}
+	
+	// Update is called once per frame
+	void Update () 
     {
         if (health <= 0)
         {
             Destroy(gameObject);
         }
 	}
-	
-	// Update is called once per frame
-	void Update () 
-    {
-	
-	}
     void PlaySound(int clip)
     {
         GetComponent<AudioSource>().PlayOneShot(audioClip[clip]);
+    }
+    public void SetParticles ()
+    {
+        switch(enemyType)
+        {
+            case EnemyType.Corrosive: 
+                {
+                    for (int i = 0; i < particlePlacements.Length; i++)
+                    {
+                       GameObject parent = (GameObject) Instantiate(Resources.Load("ParticleSystem_Corrosive"), particlePlacements[i].transform.position, particlePlacements[i].transform.rotation);
+                       parent.transform.parent = transform;
+                    }
+                        break; 
+                }
+            case EnemyType.Flame:
+                {
+                    for (int i = 0; i < particlePlacements.Length; i++)
+                    {
+                        GameObject parent = (GameObject)Instantiate(Resources.Load("ParticleSystem_Flame"), particlePlacements[i].transform.position, particlePlacements[i].transform.rotation);
+                        parent.transform.parent = transform;
+                    }
+                    break;
+                }
+            case EnemyType.Electric:
+                {
+                    for (int i = 0; i < particlePlacements.Length; i++)
+                    {
+                        GameObject parent = (GameObject)Instantiate(Resources.Load("ParticleSystem_Electric"), particlePlacements[i].transform.position, particlePlacements[i].transform.rotation);
+                        parent.transform.parent = transform;
+                    }
+                    break;
+                }
+            case EnemyType.Spook:
+                {
+                    for (int i = 0; i < particlePlacements.Length; i++)
+                    {
+                        GameObject parent = (GameObject)Instantiate(Resources.Load("ParticleSystem_Spook"), particlePlacements[i].transform.position, particlePlacements[i].transform.rotation);
+                        parent.transform.parent = transform;
+                    }
+                    break;
+                }
+            case EnemyType.Crystal:
+                {
+                    for (int i = 0; i < particlePlacements.Length; i++)
+                    {
+                        GameObject parent = (GameObject)Instantiate(Resources.Load("ParticleSystem_Crystal"), particlePlacements[i].transform.position, particlePlacements[i].transform.rotation);
+                        parent.transform.parent = transform;
+                    }
+                    break;
+                }
+            default:
+                break;
+        }
     }
 }
 public enum EnemyType
