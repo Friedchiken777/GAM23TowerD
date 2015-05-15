afb165383b87e0812ea621c0eef42d0fe1276d5a
Julius Jimenez <jjimenez149@student.rcc.edu>
2015-05-14 21:59:15 -0700

    no message

--- Assets/AudioManager.cs
+++ Assets/AudioManager.cs
@@ -1,15 +1,25 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
-public class AudioManager : MonoBehaviour {
+public class AudioManager : MonoBehaviour 
+{
+	public AudioClip[] GameMusicTracks;
+	public AudioClip[] PlayerSounds;
+	public AudioClip[] TowerSounds;
+	public AudioClip[] Explosions;
+	public AudioClip[] CannonSounds;
+	public AudioClip[] WeaponSounds;
+	public AudioClip[] EnemySounds;
 
 	// Use this for initialization
-	void Start () {
+	void Start () 
+	{
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
 	
 	}
 }
